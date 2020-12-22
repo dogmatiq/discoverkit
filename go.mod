ed7e04a85d4ab72b3198aaa57bfd14247a92b34d
James Harris <mailjamesharris@gmail.com>
2020-12-23 06:32:21 +1000

    Add `Server.Check()`.

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/dogmatiq/configkit v0.9.1
-	github.com/dogmatiq/interopspec v0.4.1-0.20201222202939-f015b4ed2f04
+	github.com/dogmatiq/interopspec v0.4.1-0.20201224041540-939b2195d70c
 	github.com/dogmatiq/linger v0.2.1
 	github.com/onsi/ginkgo v1.14.2
 	github.com/onsi/gomega v1.10.4
--- server.go
+++ server.go
@@ -1,4 +1,4 @@
-package api
+package discoverkit
 
 import (
 	"sync"
@@ -11,17 +11,17 @@ import (
 type Server struct {
 	m sync.Mutex
 
-	// available is the set of applications that are currently available.
+	// available is the set of applications that are currently available indexed
+	// by their identity key.
 	//
-	// The key is the application's identity key.
-	//
-	// The map value itself is immutable. When a change to the available
-	// applications is made, the map is cloned and the changes applied to the
-	// clone. Finally, s.available is updated to refer to the clone.
+	// The map value itself is treated as though it is immutable. When a change
+	// to the available applications is made the map is cloned and the changes
+	// applied to the clone. Finally, s.available is updated to refer to the
+	// clone.
 	//
 	// This allows many goroutines to read from any given "version" of the map
 	// without holding any locks.
-	available map[string]*discoverspec.Identity
+	available map[string]struct{}
 
 	// changed is a "broadcast" channel that is closed to signal that the set of
 	// available applications has been replaced with a new "version".
@@ -57,19 +57,19 @@ func (s *Server) update(app configkit.Identity, available bool) {
 
 	// Create a clone of s.available. This avoids any data races with other
 	// goroutines reading the map currently references by s.available.
-	next := map[string]*discoverspec.Identity{}
-	for k, ident := range s.available {
-		next[k] = ident
+	next := make(map[string]struct{}, len(s.available))
+
+	// Add the newly available application.
+	if available {
+		next[app.Key] = struct{}{}
 	}
 
-	// Update the clone to reflect the change.
-	if available {
-		next[app.Key] = &discoverspec.Identity{
-			Name: app.Name,
-			Key:  app.Key,
+	// Copy the existing applications excluding the current app if it has become
+	// unavailable.
+	for k := range s.available {
+		if !available && k == app.Key {
+			next[k] = struct{}{}
 		}
-	} else {
-		delete(next, app.Key)
 	}
 
 	// Replace s.available with the clone.
@@ -82,9 +82,9 @@ func (s *Server) update(app configkit.Identity, available bool) {
 	}
 }
 
-// capture returns the current set of available applications, and a channel that
+// snapshot returns the current set of available applications, and a channel that
 // is closed if the set of available applications changes.
-func (s *Server) capture() (map[string]*discoverspec.Identity, <-chan struct{}) {
+func (s *Server) snapshot() (map[string]struct{}, <-chan struct{}) {
 	s.m.Lock()
 	defer s.m.Unlock()
 
@@ -95,23 +95,23 @@ func (s *Server) capture() (map[string]*discoverspec.Identity, <-chan struct{}) 
 	return s.available, s.changed
 }
 
-// Watch starts watching the server for updates to the availability of Dogma
-// applications.
-func (s *Server) Watch(
-	_ *discoverspec.WatchRequest,
-	stream discoverspec.DiscoverAPI_WatchServer,
+// WatchApplications starts watching the server for updates to the availability
+// of Dogma applications.
+func (s *Server) WatchApplications(
+	_ *discoverspec.WatchApplicationsRequest,
+	stream discoverspec.DiscoverAPI_WatchApplicationsServer,
 ) error {
 	// Keep a reference to the previous map of available applications. This is
 	// used to compute a "diff" when the available applications is updated.
 	//
 	// This approach is taken as it allows changes to the available applications
 	// to be applied in s.Available() and s.Unavailable() without waiting for
-	// each individual Watch() consumer to receive its responses.
-	var prev map[string]*discoverspec.Identity
+	// each individual WatchApplications() consumer to receive its responses.
+	var prev map[string]struct{}
 
 	for {
 		// Read the current list of available applications.
-		next, changed := s.capture()
+		next, changed := s.snapshot()
 
 		// Send an "available" response for each application that is in "next",
 		// but not in "prev".
@@ -139,18 +139,18 @@ func (s *Server) Watch(
 // diff sends a WatchResponse for each application that is present in lhs but
 // not present in rhs.
 func (s *Server) diff(
-	stream discoverspec.DiscoverAPI_WatchServer,
+	stream discoverspec.DiscoverAPI_WatchApplicationsServer,
 	available bool,
-	lhs, rhs map[string]*discoverspec.Identity,
+	lhs, rhs map[string]struct{},
 ) error {
-	for k, app := range lhs {
+	for k := range lhs {
 		if _, ok := rhs[k]; ok {
 			continue
 		}
 
-		res := &discoverspec.WatchResponse{
-			Application: app,
-			Available:   available,
+		res := &discoverspec.WatchApplicationsResponse{
+			ApplicationKey: k,
+			Available:      available,
 		}
 
 		if err := stream.Send(res); err != nil {
