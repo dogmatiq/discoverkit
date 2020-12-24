3acfab20847af4ba6882d081e60ae7d279fb9bce
James Harris <mailjamesharris@gmail.com>
2020-12-24 10:26:33 +1000

    Add tests for `DiscoverObserverError`.

--- discoverer_test.go
+++ discoverer_test.go
@@ -2,9 +2,36 @@ package discoverkit_test
 
 import (
 	"context"
+	"errors"
 
 	. "github.com/dogmatiq/discoverkit"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
+
+var _ = Describe("type DiscoverObserverError", func() {
+	Describe("func Error()", func() {
+		It("provides context about the target", func() {
+			err := DiscoverObserverError{
+				Target: Target{Name: "<target>"},
+				Cause:  errors.New("<error>"),
+			}
+
+			Expect(err.Error()).To(Equal("failure observing '<target>' target: <error>"))
+		})
+	})
+
+	Describe("func Unwrap()", func() {
+		It("unwraps the causal error", func() {
+			cause := errors.New("<error>")
+			err := DiscoverObserverError{
+				Cause: cause,
+			}
+
+			Expect(errors.Is(err, cause)).To(BeTrue())
+		})
+	})
+})
 
 // discoverObserverStub is a test implementation of the TargetObserver interface.
 type discoverObserverStub struct {
