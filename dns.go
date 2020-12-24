72c4200db440fa784278154be1d1e6a9b614cbe7
James Harris <mailjamesharris@gmail.com>
2020-12-24 10:13:46 +1000

    Refactor `DNSDiscoverer` to use `DiscoverObserver`.

--- dns.go
+++ dns.go
@@ -3,9 +3,11 @@ package discoverkit
 import (
 	"context"
 	"net"
+	"strings"
 	"time"
 
 	"github.com/dogmatiq/linger"
+	"golang.org/x/sync/errgroup"
 )
 
 // DNSResolver is an interface for the subset of net.Resolver used by
@@ -37,7 +39,7 @@ type DNSDiscoverer struct {
 	// If NewTargets is nil the discoverer constructs a single Target for each
 	// discovered address. The target name is the discovered address and no
 	// explicit port is specified.
-	NewTargets func(ctx context.Context, addr string) (targets []*Target, err error)
+	NewTargets func(ctx context.Context, addr string) (targets []Target, err error)
 
 	// Resolver is the DNS resolver used to make queries.
 	//
@@ -49,31 +51,52 @@ type DNSDiscoverer struct {
 	// If it is non-positive, the DefaultDNSQueryInterval constant is used.
 	QueryInterval time.Duration
 
-	// targets is the set of targets currently known to the discoverer.
-	targets map[string][]*Target
+	parent   context.Context
+	group    *errgroup.Group
+	known    map[string]context.CancelFunc
+	observer DiscoverObserver
 }
 
-// Discover notifies o of targets that are discovered or undiscovered until ctx
-// is canceled or an error occurs.
-func (d *DNSDiscoverer) Discover(ctx context.Context, o TargetObserver) error {
-	defer func() {
-		for _, targets := range d.targets {
-			for _, t := range targets {
-				o.TargetUndiscovered(t)
-			}
-		}
-	}()
+// Discover invokes o.TargetDiscovered() when a new target is discovered.
+//
+// Each invocation is made on its own goroutine. The context passed to
+// o.TargetDiscovered() is canceled when the target is "undiscovered", or the
+// discoverer itself is stopped due to cancelation of ctx.
+//
+// The discoverer stops and returns a DiscoverObserverError if any call to
+// o.TargetDiscovered() returns a non-nil error.
+func (d *DNSDiscoverer) Discover(ctx context.Context, o DiscoverObserver) error {
+	d.group, d.parent = errgroup.WithContext(ctx)
+	d.known = map[string]context.CancelFunc{}
+	d.observer = o
 
+	d.group.Go(func() error {
+		// Perform the actual discovery within the same group as the observer
+		// goroutines. This ensures both that Wait() always has something to
+		// wait for, so that it doesn't just return immediately, and that the
+		// whole group is shutdown if the discovery process itself fails.
+		return d.discover(d.parent)
+	})
+
+	return d.group.Wait()
+}
+
+// discover periodically queries the DNS server and starts/stops observer
+// goroutines as necessary.
+func (d *DNSDiscoverer) discover(ctx context.Context) error {
 	for {
-		addrs, err := d.query(ctx)
+		// Perform the DNS query.
+		results, err := d.query(ctx)
 		if err != nil {
 			return err
 		}
 
-		if err := d.update(ctx, addrs, o); err != nil {
+		// Start and stop observer goroutines to match the new results.
+		if err := d.sync(ctx, results); err != nil {
 			return err
 		}
 
+		// Wait until it's time to perform the next DNS query.
 		if err := linger.Sleep(
 			ctx,
 			d.QueryInterval,
@@ -84,59 +107,60 @@ func (d *DNSDiscoverer) Discover(ctx context.Context, o TargetObserver) error {
 	}
 }
 
-// update sends notifications to o about the targets that have become
-// available/unavailable based on new query results.
-func (d *DNSDiscoverer) update(
+// sync synchronizes the state of running observers based on a new set of DNS
+// query results.
+func (d *DNSDiscoverer) sync(
 	ctx context.Context,
-	addrs []string,
-	o TargetObserver,
+	results map[string]struct{},
 ) error {
-	prev := d.targets
-	d.targets = make(map[string][]*Target, len(addrs))
-
-	// Add an entry to d.targets for each of the addresses in the query result.
-	for _, addr := range addrs {
-		if targets, ok := prev[addr]; ok {
-			// We have already discovered this address. It may or may not have
-			// any targets. Either way we retain them unchanged.
-			d.targets[addr] = targets
-			continue
+	// First we check through the known addresses to work out which ones are
+	// still in the latest query results.
+	for addr, cancel := range d.known {
+		if _, ok := results[addr]; ok {
+			// This address is still avaliable. Remove it from the query results
+			// so we're left only with addresses that we have not seen before.
+			delete(results, addr)
+		} else {
+			// This address is no longer available. Cancel the associated
+			// context to stop the observer goroutines.
+			delete(d.known, addr)
+			cancel()
 		}
+	}
 
-		// Otherwise, we're seeing this address for the first time, or it's
-		// reappeared after being removed from the DNS results. We construct the
-		// targets that are at this address.
+	// Then we can look at the query results, which at this point contains only
+	// those addresses we didn't already know about.
+	for addr := range results {
 		targets, err := d.newTargets(ctx, addr)
 		if err != nil {
 			return err
 		}
 
-		// Notify the observer of the targets.
+		// Create a new context specifically for this address. Note that it is
+		// derived directly from d.parent, not ctx (which represents only the
+		// lifetime of this call to the update() method). It will be canceled if
+		// the address dissappears from the query results.
+		addrCtx, cancel := context.WithCancel(d.parent)
+		d.known[addr] = cancel
+
+		// Start a new goroutine for each target.
 		for _, t := range targets {
-			o.TargetDiscovered(t)
-		}
-
-		// Store the discovered targets against the address.
-		// Note that we still store a nil or empty slice against this
-		// address. This ensures that we still treat the address as "seen".
-		d.targets[addr] = targets
-	}
-
-	// Notify the observer of any targets that have gone away because the
-	// address they were based on is no longer in the query results.
-	for a, targets := range prev {
-		if _, ok := d.targets[a]; !ok {
-			for _, dt := range targets {
-				o.TargetUndiscovered(dt)
-			}
+			t := t // capture loop variable
+			d.group.Go(func() error {
+				return targetDiscovered(addrCtx, d, d.observer, t)
+			})
 		}
 	}
 
 	return nil
 }
 
-// query performs a DNS query to find API targets.
-func (d *DNSDiscoverer) query(ctx context.Context) ([]string, error) {
+// query performs a DNS query to A, AAAA and CNAME records associated with
+// d.QueryHost.
+//
+// It returns the resulting addresses as a set with names transformed to
+// lowercase. Individual addresses may be hostnames or IP addresses.
+func (d *DNSDiscoverer) query(ctx context.Context) (map[string]struct{}, error) {
 	r := d.Resolver
 	if r == nil {
 		r = net.DefaultResolver
@@ -145,6 +169,8 @@ func (d *DNSDiscoverer) query(ctx context.Context) ([]string, error) {
 	addrs, err := r.LookupHost(ctx, d.QueryHost)
 	if err != nil {
 		if x, ok := err.(*net.DNSError); ok {
+			// Temporary network problems, or the fact that host doesn't exist
+			// *right now* are not errors that should stop the discoverer.
 			if x.IsTemporary || x.IsNotFound {
 				return nil, nil
 			}
@@ -153,16 +179,22 @@ func (d *DNSDiscoverer) query(ctx context.Context) ([]string, error) {
 		return nil, err
 	}
 
-	return addrs, nil
+	// Convert the slice of addresses to a set of lowercase strings.
+	results := make(map[string]struct{}, len(addrs))
+	for _, addr := range addrs {
+		results[strings.ToLower(addr)] = struct{}{}
+	}
+
+	return results, err
 }
 
 // newTarget returns the targets at the given address.
-func (d *DNSDiscoverer) newTargets(ctx context.Context, addr string) ([]*Target, error) {
+func (d *DNSDiscoverer) newTargets(ctx context.Context, addr string) ([]Target, error) {
 	if d.NewTargets != nil {
 		return d.NewTargets(ctx, addr)
 	}
 
-	return []*Target{
+	return []Target{
 		{Name: addr},
 	}, nil
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/dogmatiq/linger v0.2.1
 	github.com/onsi/ginkgo v1.14.2
 	github.com/onsi/gomega v1.10.4
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	google.golang.org/grpc v1.34.0
 )
